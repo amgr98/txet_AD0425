b5131682946d0b2fdc66796713a6f67110b23320
اسماء قمر <AD0425@iu.edu.jo>
2022-06-12 16:47:00 +0300

    Add project files.

---
+++ txet_AD0425/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace txet_AD0425
+{
+    partial class txtTextEditor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(txtTextEditor));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnFontColor = new System.Windows.Forms.Button();
+            this.btnChangeFormColor = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnFontD = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(762, 540);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pastToolStripMenuItem,
+            this.clearAllToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 128);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.cutToolStripMenuItem.Text = "cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.copyToolStripMenuItem.Text = "copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // pastToolStripMenuItem
+            // 
+            this.pastToolStripMenuItem.Name = "pastToolStripMenuItem";
+            this.pastToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.pastToolStripMenuItem.Text = "past";
+            this.pastToolStripMenuItem.Click += new System.EventHandler(this.pastToolStripMenuItem_Click);
+            // 
+            // btnFontColor
+            // 
+            this.btnFontColor.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnFontColor.Font = new System.Drawing.Font("Hyundai Sans Text Office", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFontColor.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnFontColor.Location = new System.Drawing.Point(134, 54);
+            this.btnFontColor.Name = "btnFontColor";
+            this.btnFontColor.Size = new System.Drawing.Size(108, 42);
+            this.btnFontColor.TabIndex = 1;
+            this.btnFontColor.Text = "Font Color";
+            this.btnFontColor.UseVisualStyleBackColor = false;
+            this.btnFontColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnChangeFormColor
+            // 
+            this.btnChangeFormColor.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnChangeFormColor.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnChangeFormColor.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnChangeFormColor.Location = new System.Drawing.Point(279, 54);
+            this.btnChangeFormColor.Name = "btnChangeFormColor";
+            this.btnChangeFormColor.Size = new System.Drawing.Size(215, 42);
+            this.btnChangeFormColor.TabIndex = 2;
+            this.btnChangeFormColor.Text = "Form Color";
+            this.btnChangeFormColor.UseVisualStyleBackColor = false;
+            this.btnChangeFormColor.Click += new System.EventHandler(this.btnChangeFormColor_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnSave.BackgroundImage = global::txet_AD0425.Properties.Resources._2639912_save_icon;
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSave.Location = new System.Drawing.Point(684, 658);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 72);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFontD
+            // 
+            this.btnFontD.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnFontD.Font = new System.Drawing.Font("Hyundai Sans Text Office", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFontD.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnFontD.Location = new System.Drawing.Point(522, 54);
+            this.btnFontD.Name = "btnFontD";
+            this.btnFontD.Size = new System.Drawing.Size(108, 42);
+            this.btnFontD.TabIndex = 4;
+            this.btnFontD.Text = "Font ";
+            this.btnFontD.UseVisualStyleBackColor = false;
+            this.btnFontD.Click += new System.EventHandler(this.btnFontD_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnOpen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOpen.BackgroundImage")));
+            this.btnOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOpen.Location = new System.Drawing.Point(603, 658);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 72);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.clearAllToolStripMenuItem.Text = "clear all";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // txtTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 752);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnFontD);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnChangeFormColor);
+            this.Controls.Add(this.btnFontColor);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "txtTextEditor";
+            this.Text = "Text Editor";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnFontColor;
+        private System.Windows.Forms.Button btnChangeFormColor;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFontD;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pastToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+    }
+}
---
+++ txet_AD0425/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txet_AD0425
+{
+    public partial class txtTextEditor : Form
+    {
+        public txtTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void btnChangeFormColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            DialogResult result = saveFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog.FileName);
+                MessageBox.Show("The file has been saved successfully");
+            }
+        }
+
+        private void btnFontD_Click(object sender, EventArgs e)
+        {
+            FontDialog fntDlg = new FontDialog();
+            DialogResult result = fntDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fntDlg.Font;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string fileName = getOpenFileDialog();
+            if (fileName.Length > 0)
+                richTextBox1.LoadFile(fileName);
+        }
+        private string getOpenFileDialog()
+        {
+            string rtnValue = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                rtnValue = openFileDialog.FileName;
+            }
+            return rtnValue;
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText);
+            richTextBox1.SelectedText=String.Empty;
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText);
+
+        }
+
+        private void pastToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string xx =Clipboard.GetText();
+            richTextBox1.Text=richTextBox1.Text.Insert(richTextBox1.SelectionStart,xx);
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+          
+}
+    
